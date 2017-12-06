79646403c3d3423932e7db599d4e05918ea8b1e0
Mikira Sora <mikirasora0409@126.com>
2017-12-06 15:59:30 +0800

    changed playing status trigger condition

--- OsuLiveStatusPanel/SourcesWrapper/MemoryReaderWrapper.cs
+++ OsuLiveStatusPanel/SourcesWrapper/MemoryReaderWrapper.cs
@@ -33,15 +33,39 @@ namespace OsuLiveStatusPanel
         {
             beatmapSetID = beatmap.BeatmapSetID;
         }
+
+        /*
+         因为MemoryReader扫Status比Mod快,如果使用Status来做是否开始打图的依据会无法及时获取当前MOD的信息.因此按Mod来判断是否开始打图,null为非打图状态
+        */
         
         public void OnCurrentModsChange(ModsInfo mod)
         {
+            if (current_mod == mod) return;
+
             current_mod = mod;
-            IO.CurrentIO.WriteColor($"mod change : {mod.ShortName}", ConsoleColor.Blue);
+
+            if (mod==null)
+            {
+                //Not Playing
+                RefPlugin.OnBeatmapChanged(null);
+            }
+            else
+            {
+                //Start to play
+                BeatmapEntry beatmap = new BeatmapEntry()
+                {
+                    BeatmapId = beatmapID,
+                    BeatmapSetId = beatmapSetID,
+                    OsuFilePath = OsuFilePath
+                };
+
+                RefPlugin.OnBeatmapChanged(new BeatmapChangedParameter() { beatmap = beatmap });
+            }
         }
 
         public void OnStatusChange(OsuStatus last_status, OsuStatus status)
         {
+            /*
             if (last_status == status) return;
             if (status != OsuStatus.Playing)
             {
@@ -59,6 +83,7 @@ namespace OsuLiveStatusPanel
                 
                 RefPlugin.OnBeatmapChanged(new BeatmapChangedParameter() { beatmap= beatmap });
             }
+            */
         }
     }
 }
