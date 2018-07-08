33603fcfa5d62f0f25f26f680a948c81b0d4b671
Mikira Sora <mikirasora0409@126.com>
2018-07-09 00:09:19 +0800

    support source DPMP

---
+++ OsuLiveStatusPanel/SourcesWrapper/DPMP/DifficultParamModifyPluginSourceWrapper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DifficultParamModifyPlugin;
+
+namespace OsuLiveStatusPanel.SourcesWrapper.DPMP
+{
+    class DifficultParamModifyPluginSourceWrapper : SourceWrapperBase<DifficultParamModifyPlugin.DifficultParamModifyPlugin>
+    {
+        public Mods.ModsInfo CurrentMod
+        {
+            get
+            {
+                return new Mods.ModsInfo() {
+                Mod= (Mods.ModsInfo.Mods)((uint)RefPlugin.CurrentMods.Mod)
+                };
+            }
+        }
+
+        public DifficultParamModifyPluginSourceWrapper(DifficultParamModifyPlugin.DifficultParamModifyPlugin ref_plugin, OsuLiveStatusPanelPlugin plugin) : base(ref_plugin, plugin)
+        {
+
+        }
+
+        private void RefPlugin_OnBeatmapChanged(string osu_file, int set_id, int id, bool output_type)
+        {
+            RefPanelPlugin.OnBeatmapChanged(this, new BeatmapChangedParameter() {
+                beatmap = new BeatmapEntry()
+                {
+                    BeatmapId = id,
+                    BeatmapSetId = set_id,
+                    OsuFilePath=osu_file,
+                    OutputType=output_type?OutputType.Play:OutputType.Listen
+                }
+            });
+        }
+
+        public override bool Attach()
+        {
+            this.RefPlugin.OnBeatmapChanged += RefPlugin_OnBeatmapChanged;
+            return true;
+        }
+
+        public override void Detach()
+        {
+            this.RefPlugin.OnBeatmapChanged -= RefPlugin_OnBeatmapChanged;
+        }
+    }
+}
